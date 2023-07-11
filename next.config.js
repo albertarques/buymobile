4b65e157be7506a5651d93e9183b33ca8d526722
Albert Arques <albertarques@gmail.com>
2023-07-11 11:41:16 +0200

    Added origin images url to Next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['itx-frontend-test.onrender.com']
+      }    
+}
 
 module.exports = nextConfig
