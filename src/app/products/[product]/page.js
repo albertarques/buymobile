0dcb96fc41dee4bf944cfc3f18fe7c76424ba100
Albert Arques <albertarques@gmail.com>
2023-07-11 22:26:50 +0200

    Added breadcrumb as a unordered list

--- src/app/products/[product]/page.js
+++ src/app/products/[product]/page.js
@@ -1,19 +1,29 @@
-import Image from "next/image";
 import ProductDetail from "@/components/products/ProductDetail";
+import Link from "next/link";
 
 const fetchProduct = async (id) => {
   const res = await fetch("https://itx-frontend-test.onrender.com/api/product/" + id);
   const data = await res.json();
+  console.log(data)
   return data;
 };
 
 export default async function Product({ params }) {
 
-  console.log(params)
   const product = await fetchProduct(params.product)
 
   return (
-    <main className="flex container flex-col relative">
+    <main className="container">
+      <ul className="flex">
+        <li className="after:content-['_->_']">
+          <Link href="/" className="text-blue-500">
+            Home
+          </Link>
+        </li>
+        <li className="before:content-['_']">
+          {product.model}
+        </li>
+      </ul>
       <ProductDetail product={product} />
     </main>
   );
