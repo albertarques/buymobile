71e8a40f8ef26a3c349536d81761ec73d0f3f860
Albert Arques <albertarques@gmail.com>
2023-07-11 22:28:10 +0200

    Add AddProductButton to ProductDetail page

--- src/components/products/ProductDetail.jsx
+++ src/components/products/ProductDetail.jsx
@@ -1,30 +1,30 @@
 import Image from "next/image";
 import Link from "next/link";
+import AddProductButton from "./AddProductButton";
 
-export default function ProductDetail({ product }) {
+export default function ProductDetail({product}) {
   return (
-    <div className="container">
-      <div className="row justify-end">
-        <Link href="/" className="text-blue-500 underline ">
-          Back to list
-        </Link>
-      </div>
+    <div className="container mx-auto px-4">
       <div className="bg-white rounded-2xl p-5 my-8 mx-4 md:w-4/5 md:mx-auto md:grid md:grid-cols-2 md:gap-4">
-        <div className="flex justify-center">
+        <Link href="/" className="inline-block h-fit col-span-2 w-full row underline ">
+          <button className="rounded-xl bg-red-500 text-white px-3 py-2">
+            Back to list
+          </button>
+        </Link>
           <Image
+            className="mx-auto"
             src={product.imgUrl}
             alt={product.model}
             width={300}
             height={250}
           />
-        </div>
         <div>
           <p>{product.brand}</p>
           <h2 className="font-bold">{product.model}</h2>
           <p>Price: {product.price}€</p>
+          <br />
           <h3 className="font-bold">Description</h3>
           <p>CPU: {product.cpu}</p>
-          <br />
           <p>RAM: {product.ram}</p>
           <p>Sistema Operativo: {product.os}</p>
           <p>Resolución de pantalla: {product.displayResolution}</p>
@@ -33,7 +33,7 @@ export default function ProductDetail({ product }) {
           <p>Cámara frontal: {product.secondaryCamera}</p>
           <p>Dimensiones: {product.dimentions}</p>
           <p>Peso: {product.weight} grs.</p>
-          <button>Añadir al carrito</button>
+          <AddProductButton product={product} />
         </div>
       </div>
     </div>
