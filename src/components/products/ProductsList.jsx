76fb464c44ec2399d899ab0fe779959560b9f03c
Albert Arques <albertarques@gmail.com>
2023-07-11 11:44:25 +0200

    Added ProductList component

---
+++ src/components/products/ProductsList.jsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useState } from "react";
+import ProductCard from "./ProductCard";
+
+export default function ProductsList({ products }) {
+
+  const [filteredProducts, setFilteredProducts] = useState(products);
+
+  return (
+    <>
+      {/* TODO: Make a component with SearchBar */}
+      <div className="flex flex-col p-4 w-full my-2">
+        <label 
+          className="text-xl font-bold align-middle md:py-2 md:mx-2 md:align-middle md:inline-block"
+          htmlFor="searchInput">
+          Search:
+        </label>
+        <input
+          id="searchInput"
+          className="form-control py-2 px-4 rounded shadow w-full md:w-max-content"
+          type="text"
+          placeholder="Type model name or brand..."
+          onChange={(e) => {
+            const keyword = e.target.value;
+            if (keyword !== "") {
+              const results = filteredProducts.filter((filteredProducts) => {
+                return (
+                  (filteredProducts.model).toLowerCase().includes(keyword.toLowerCase())
+                  || (filteredProducts.brand).toLowerCase().includes(keyword.toLowerCase())
+                  );
+              });
+              setFilteredProducts(results);
+            } else {
+              setFilteredProducts(products);
+            }
+          }}/>
+      </div>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4 h-full py-10">
+        {filteredProducts.map((product) => (
+          <ProductCard product={product} key={product.id}/>
+        ))}
+      </div>
+    </>
+  );
+}
