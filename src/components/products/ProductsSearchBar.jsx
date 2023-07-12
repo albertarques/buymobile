cb3bf4e588ced5a1b8558ff515779fe0c6547bef
Albert Arques <albertarques@gmail.com>
2023-07-12 16:19:13 +0200

    Added SearchBar component

---
+++ src/components/products/ProductsSearchBar.jsx
@@ -0,0 +1,41 @@
+'use client'
+
+export default function ProductsSearchBar({ filteredProducts, setFilteredProducts, products}) {
+
+  console.log(filteredProducts)
+
+  return (
+    <div className="container px-4 flex flex-col md:flex-row my-2 justify-end">
+      <label
+        className="text-xl font-bold align-middle md:py-2 md:mx-2 md:align-middle md:inline-block"
+        htmlFor="searchInput"
+      >
+        Search:
+      </label>
+      <input
+        id="searchInput"
+        className="form-control py-2 px-4 rounded shadow md:w-max-content"
+        type="text"
+        placeholder="Type model name or brand..."
+        onChange={(e) => {
+          const keyword = e.target.value;
+          if (keyword !== "" && filteredProducts.length > 0) {
+            const results = filteredProducts.filter((filteredProducts) => {
+              return (
+                filteredProducts.model
+                  .toLowerCase()
+                  .includes(keyword.toLowerCase()) ||
+                filteredProducts.brand
+                  .toLowerCase()
+                  .includes(keyword.toLowerCase())
+              );
+            });
+            setFilteredProducts(results);
+          } else {
+            setFilteredProducts(products);
+          }
+        }}
+      />
+    </div>
+  );
+}
