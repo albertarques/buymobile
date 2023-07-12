45c3c2ba4cc75c68dfb873dfda25b6bd18a2203e
Albert Arques <albertarques@gmail.com>
2023-07-12 20:03:41 +0200

    Added AddToLocalStorageKey and CartContext functions

--- src/components/products/AddProductButton.jsx
+++ src/components/products/AddProductButton.jsx
@@ -1,26 +1,34 @@
 "use client";
 
-function handlerAddProductToLocalstorage(product) {
-  const color = document.getElementById("color").value;
-  const storage = document.getElementById("storage").value;
-
-  const productExcerp = {
-    id: product.id,
-    color: color,
-    storage: storage,
-  };
-  let cart = JSON.parse(localStorage.getItem("cart")) || [];
-  let productInCart = cart.find((p) => p.id === product.id);
-  cart.push(productExcerp);
-  localStorage.setItem("cart", JSON.stringify(cart));
-}
+import { useContext } from "react";
+import { CartContext } from "../context/CartContext";
+import AddToLocalStorageKey from "../service/AddToLocalStorageKey";
 
 export default function AddProductButton({ product }) {
+
+  const { setCartContext } = useContext(CartContext);
+
+  function handlerAddToCart(product) {
+
+    const color = document.getElementById("color").value;
+    const storage = document.getElementById("storage").value;
+  
+    const productValues = {
+      id: product.id,
+      color: color,
+      storage: storage,
+    };
+  
+    AddToLocalStorageKey("cart", productValues)
+    setCartContext({cart: JSON.parse(localStorage.getItem("cart"))})
+    return localStorage.getItem("cart")
+  }
+  
   if (product.price != "" && product.price != "0" && product.price != null) {
     return (
       <button
         className="bg-blue-500 rounded-xl px-4 py-2 text-white my-4"
-        onClick={() => handlerAddProductToLocalstorage(product)}
+        onClick={() => handlerAddToCart(product)}
       >
         Añadir al carrito
       </button>
