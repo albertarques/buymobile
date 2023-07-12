c826bf0cea5a94bf96d68b2681fee238b35d08dd
Albert Arques <albertarques@gmail.com>
2023-07-12 10:34:40 +0200

    Added color and storage properties

--- src/components/products/AddProductButton.jsx
+++ src/components/products/AddProductButton.jsx
@@ -1,24 +1,23 @@
 'use client'
 
 function handlerAddProductToLocalstorage(product) {
-    //TODO: Add product to localStorage and quantity and update the cart icon
-    console.log(product)
+    const color = document.getElementById('color').value
+    const storage = document.getElementById('storage').value
+
+    const productExcerp = {
+        id: product.id,
+        color: color,
+        storage: storage
+    }
     let cart = JSON.parse(localStorage.getItem('cart')) || []
     let productInCart = cart.find(p => p.id === product.id)
-    if (productInCart) {
-        productInCart.quantity += 1
-        }
-    else {
-        product.quantity = 1
-        cart.push(product)
-        }
+        cart.push(productExcerp)
     localStorage.setItem('cart', JSON.stringify(cart))
   }
   
 export default function AddProductButton({product}) {
     return (
         <button className="bg-blue-500 rounded-xl px-4 py-2 text-white my-4"
-        // TODO: save the product on localStorage
         onClick={() => handlerAddProductToLocalstorage(product)}
       >Buy one</button>
 );
