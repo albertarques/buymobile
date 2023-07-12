8dc39e4061ee7ff10536dbcd692ff6575a4f7432
Albert Arques <albertarques@gmail.com>
2023-07-12 16:18:59 +0200

    Added "product not found" if search don't return any

--- src/components/products/ProductsList.jsx
+++ src/components/products/ProductsList.jsx
@@ -2,45 +2,43 @@
 
 import { useState } from "react";
 import ProductCard from "./ProductCard";
+import ProductsSearchBar from "./ProductsSearchBar";
 
 export default function ProductsList({ products }) {
-
   const [filteredProducts, setFilteredProducts] = useState(products);
+
+  if (filteredProducts.length === 0) {
+    return (
+      <>
+        <ProductsSearchBar
+          filteredProducts={filteredProducts}
+          setFilteredProducts={setFilteredProducts}
+          products={products}/>
+        <div className="container px-4 mx-auto h-full py-10">
+          <div className="flex flex-col items-center justify-center">
+            <h2 className="text-3xl font-bold text-center">
+              No hemos encontrado este producto!
+            </h2>
+            <p className="text-xl text-center">
+              Intentalo con un nombre de producto distinto.
+            </p>
+          </div>
+        </div>
+      </>
+    );
+  }
 
   return (
     <>
-      {/* TODO: Make a component with SearchBar */}
-      <div className="container px-4 flex flex-col md:flex-row my-2 justify-end">
-        <label 
-          className="text-xl font-bold align-middle md:py-2 md:mx-2 md:align-middle md:inline-block"
-          htmlFor="searchInput">
-          Search:
-        </label>
-        <input
-          id="searchInput"
-          className="form-control py-2 px-4 rounded shadow md:w-max-content"
-          type="text"
-          placeholder="Type model name or brand..."
-          onChange={(e) => {
-            const keyword = e.target.value;
-            if (keyword !== "") {
-              const results = filteredProducts.filter((filteredProducts) => {
-                return (
-                  (filteredProducts.model).toLowerCase().includes(keyword.toLowerCase())
-                  || (filteredProducts.brand).toLowerCase().includes(keyword.toLowerCase())
-                  );
-              });
-              setFilteredProducts(results);
-            } else {
-              setFilteredProducts(products);
-            }
-          }}/>
-      </div>
+      <ProductsSearchBar
+        filteredProducts={filteredProducts}
+        setFilteredProducts={setFilteredProducts}
+        products={products}
+      />
       <div className="container px-4 grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4 h-full py-10">
         {filteredProducts.map((product) => (
-            <ProductCard product={product} key={product.id}/>
-          ))
-        }
+          <ProductCard product={product} key={product.id} />
+        ))}
       </div>
     </>
   );
