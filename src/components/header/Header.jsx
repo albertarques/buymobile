24b5c4d43039bd5dbf656b3fa2f4a9b0eb09a22a
Albert Arques <albertarques@gmail.com>
2023-07-12 10:32:24 +0200

    Added Header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,17 @@
+import Cart from "./Cart";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <div className="bg-blue-500">
+      <header className="container flex mx-auto font-bold text-2xl text-white justify-between">
+        <Link href="/" className="top-0 w-full left-0 p-4 z-10">
+          <h1>
+            90's Mobiles
+          </h1>
+        </Link>
+        <Cart />
+      </header>
+    </div>
+  );
+}
