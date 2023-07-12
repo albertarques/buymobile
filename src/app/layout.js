104dc442ccea570c08382d7aebbd32889e5afed0
Albert Arques <albertarques@gmail.com>
2023-07-12 16:20:57 +0200

    Refactored language to es

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,13 +5,14 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: '90\'s Mobile',
-  description: 'Your retro smartphone shop ',
+  title: 'Mobiles retro',
+  description: 'Tu tienda de móviles retro',
 }
 
 export default function RootLayout({ children }) {
+
   return (
-    <html lang="en">
+    <html lang="es">
       <body className={[inter.className + " justify-center"]}>
         <Header />
         {children}
