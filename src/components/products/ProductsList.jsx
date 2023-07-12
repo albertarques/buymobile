4036b937899324ddf8dc4936f40924a2f74fed15
Albert Arques <albertarques@gmail.com>
2023-07-12 10:37:57 +0200

    Refactored some styles

--- src/components/products/ProductsList.jsx
+++ src/components/products/ProductsList.jsx
@@ -10,7 +10,7 @@ export default function ProductsList({ products }) {
   return (
     <>
       {/* TODO: Make a component with SearchBar */}
-      <div className="flex flex-col p-4 w-full my-2">
+      <div className="container px-4 flex flex-col md:flex-row my-2 justify-end">
         <label 
           className="text-xl font-bold align-middle md:py-2 md:mx-2 md:align-middle md:inline-block"
           htmlFor="searchInput">
@@ -18,7 +18,7 @@ export default function ProductsList({ products }) {
         </label>
         <input
           id="searchInput"
-          className="form-control py-2 px-4 rounded shadow w-full md:w-max-content"
+          className="form-control py-2 px-4 rounded shadow md:w-max-content"
           type="text"
           placeholder="Type model name or brand..."
           onChange={(e) => {
@@ -36,10 +36,11 @@ export default function ProductsList({ products }) {
             }
           }}/>
       </div>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4 h-full py-10">
+      <div className="container px-4 grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4 h-full py-10">
         {filteredProducts.map((product) => (
-          <ProductCard product={product} key={product.id}/>
-        ))}
+            <ProductCard product={product} key={product.id}/>
+          ))
+        }
       </div>
     </>
   );
