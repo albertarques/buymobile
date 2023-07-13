89249bc73cd78aabb1d249302b4bc8c25c8890af
Albert Arques <albertarques@gmail.com>
2023-07-13 08:20:26 +0200

    Add breadcrumb to home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import ProductsList from "@/components/products/ProductsList";
+import Link from "next/link";
 
 const fetchProducts = async () => {
   const res = await fetch(
@@ -13,7 +14,15 @@ export default async function Home() {
   const products = await fetchProducts();
 
   return (
-    <main className="flex container min-h-screen flex-col items-center relative top-14">
+    <main className="flex container min-h-screen flex-col items-center relative">
+      <ul className="flex p-4 w-full">
+        <li>
+          <Link href="/" className="text-blue-500">
+            Inicio
+          </Link>
+        </li>
+      </ul>
+
       <ProductsList products={products} />
     </main>
   );
