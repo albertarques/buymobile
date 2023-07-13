a7e0330076420bd7edf506ec4d236ad2f396dad0
Albert Arques <albertarques@gmail.com>
2023-07-13 08:22:21 +0200

    Refactor the position of h1 tag

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,11 +5,11 @@ export default function Header({cartNumberItems}) {
   return (
     <div className="bg-blue-500">
       <header className="container flex mx-auto font-bold text-2xl text-white justify-between">
-        <Link href="/" className="top-0 w-full left-0 p-4 z-10">
-          <h1>
-            Mobiles retro
-          </h1>
-        </Link>
+        <h1 className="py-4">
+          <Link href="/" className="top-0 w-full left-0 p-4 z-10">
+              Mobiles retro
+          </Link>
+        </h1>
         <Cart />
       </header>
     </div>
