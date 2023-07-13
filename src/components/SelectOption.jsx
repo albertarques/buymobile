f6d8e4865ccbaf26c920997cb2ba85532ceee2fc
Albert Arques <albertarques@gmail.com>
2023-07-13 08:22:00 +0200

    Refactor the code of option that will be saved on localStorage

--- src/components/SelectOption.jsx
+++ src/components/SelectOption.jsx
@@ -5,7 +5,7 @@ export default function SelectOption({options, label, id}) {
       <label className="mt-2 mr-4 w-full" htmlFor={id}>{label}</label>
       <select id={id} className="w-full bg-gray-200 rounded-xl px-4 py-2 text-gray-700 my-2 md:mt-0">
         {options.map((option) => (
-          <option key={option.name} value={option.name}>
+          <option key={option.name} value={option.code}>
             {option.name}
           </option>
         ))}
