5129474d0a8ca4b8dbabb29dab1c8d8698f02eb3
Albert Arques <albertarques@gmail.com>
2023-07-14 10:44:10 +0200

    Added error message to user if fetch has failed

--- src/app/page.js
+++ src/app/page.js
@@ -2,16 +2,33 @@ import ProductsList from "@/components/products/ProductsList";
 import Link from "next/link";
 
 const fetchProducts = async () => {
-  const res = await fetch(
-    "https://itx-frontend-test.onrender.com/api/product",
-    { next: { revalidate: 3600 } }
-  );
-  const data = await res.json();
-  return data;
+  try {
+    const res = await fetch(
+      "https://itx-frontend-test.onrender.com/api/product",
+      { next: { revalidate: 3600 } }
+    );
+    const data = await res.json();
+    return data;  
+  } catch (error) {
+    console.log(error);  }
 };
 
 export default async function Home() {
   const products = await fetchProducts();
+
+  if(!products) return (
+    <main className="flex container min-h-screen flex-col items-center relative">
+      <ul className="flex p-4 w-full">
+        <li>
+          <Link href="/" className="text-blue-500">
+            Inicio
+          </Link>
+        </li>
+      </ul>
+      <h1 className="text-2xl">Error al mostrar los productos!</h1>
+      <p>Por favor, intentelo nuevamente más tarde.</p>
+    </main>
+  );
 
   return (
     <main className="flex container min-h-screen flex-col items-center relative">
