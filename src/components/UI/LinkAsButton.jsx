9a8bda137976d4e160ea8ef47a763632675b6af4
Albert Arques <albertarques@gmail.com>
2023-07-14 08:14:14 +0200

    Added LinkAsButton component

---
+++ src/components/UI/LinkAsButton.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function ({text, route, bgColor, textColor}) {
+  return (
+    <Link
+      href={route}
+      className={[`bg-${bgColor}-500 hover:bg-${bgColor}-700 active:bg-${bgColor}-900 rounded-xl text-${textColor} px-3 py-2 inline-block h-fit col-span-2 ml-4`]}>
+        {text}
+    </Link>
+  );
+}
