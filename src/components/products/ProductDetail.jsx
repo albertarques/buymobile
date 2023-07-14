f3226010e96516cc8f4588e6c246deb6dd3a0ac0
Albert Arques <albertarques@gmail.com>
2023-07-14 08:46:01 +0200

    Deleted unused Link import

--- src/components/products/ProductDetail.jsx
+++ src/components/products/ProductDetail.jsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Link from "next/link";
 import AddProductButton from "./AddProductButton";
 import SelectOption from "../SelectOption";
 import LinkAsButton from "../UI/LinkAsButton";
