6f069e846b8b7c30b13f62f37bfc396a92c136f4
Albert Arques <albertarques@gmail.com>
2023-07-14 09:14:15 +0200

    Refactor Link className properties

--- src/components/UI/LinkAsButton.jsx
+++ src/components/UI/LinkAsButton.jsx
@@ -4,7 +4,7 @@ export default function LinkAsButton({text, route, bgColor, textColor}) {
   return (
     <Link
       href={route}
-      className={`bg-${bgColor}-500 hover:bg-${bgColor}-700 active:bg-${bgColor}-900 rounded-xl text-${textColor} px-3 py-2 inline-block h-fit col-span-2 ml-4`}>
+      className={[`bg-${bgColor}-500 hover:bg-${bgColor}-700 active:bg-${bgColor}-900 rounded-xl text-${textColor} px-3 py-2 inline-block h-fit col-span-2 ml-4`]}>
         {text}
     </Link>
   );
