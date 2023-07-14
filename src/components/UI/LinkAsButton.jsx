1570d99147393c3b8a92c2a8ff3e25a751514011
Albert Arques <albertarques@gmail.com>
2023-07-14 09:19:11 +0200

    Deleted misspelling `

--- src/components/UI/LinkAsButton.jsx
+++ src/components/UI/LinkAsButton.jsx
@@ -4,7 +4,7 @@ export default function LinkAsButton({text, route}) {
   return (
     <Link
       href={route}
-      className="bg-orange-500 hover:bg-orange-700 active:bg-orange-900 rounded-xl text-white px-3 py-2 inline-block h-fit col-span-2 ml-4`">
+      className="bg-orange-500 hover:bg-orange-700 active:bg-orange-900 rounded-xl text-white px-3 py-2 inline-block h-fit col-span-2 ml-4">
         {text}
     </Link>
   );
