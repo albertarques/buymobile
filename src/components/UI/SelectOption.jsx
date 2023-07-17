812cd65beaf23ad41872c95409aca88c219d08ea
Albert Arques <albertarques@gmail.com>
2023-07-17 08:56:30 +0200

    Added some styles

--- src/components/UI/SelectOption.jsx
+++ src/components/UI/SelectOption.jsx
@@ -1,7 +1,7 @@
 'use client'
 export default function SelectOption({options, label, id}) {
   return (
-    <>
+    <div className="mr-2 my-1">
       <label className="mt-2 mr-4 w-full" htmlFor={id}>{label}</label>
       <select id={id} className=" font-sans w-full bg-gray-200 rounded-xl px-4 py-2 text-gray-700 my-2 md:mt-0">
         {options.map((option) => (
@@ -10,6 +10,6 @@ export default function SelectOption({options, label, id}) {
           </option>
         ))}
       </select>
-    </>
+    </div>
   );
 }
