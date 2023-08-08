c4d5fcabb088d6af14f9e4a016bd37194b57b478
Albert Arques <albertarques@gmail.com>
2023-08-08 08:49:23 +0200

    Added error mesage if fetch not catch products list

--- src/app/[product]/page.js
+++ src/app/[product]/page.js
@@ -1,5 +1,4 @@
 import ProductDetail from "@/components/products/ProductDetail";
-import Link from "next/link";
 
 const fetchProduct = async (id) => {
   const res = await fetch("https://itx-frontend-test.onrender.com/api/product/" + id);
@@ -8,9 +7,16 @@ const fetchProduct = async (id) => {
 };
 
 export default async function Product({ params }) {
-
+ 
   const product = await fetchProduct(params.product)
 
+  if(!product) return (
+    <main className="flex container min-h-screen flex-col items-center relative">
+      <h1 className="text-2xl">Error al mostrar los productos!</h1>
+      <p>Por favor, intentelo nuevamente más tarde.</p>
+    </main>
+  );
+        
   return (
     <main className="container">
       <ProductDetail product={product} />
